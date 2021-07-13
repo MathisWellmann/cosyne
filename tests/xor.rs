b8aedb02ea5556f9bd3a9c474a66e5a712867e05
goldenMetteyya <hello@organism.org>
2021-07-14 00:21:58 +0300

    Update xor.rs

--- tests/xor.rs
+++ tests/xor.rs
@@ -1,36 +1,36 @@
-// use cosyne::{Activation, Config, Cosyne, Environment, ANN};
+use cosyne::{Activation, Config, Cosyne, Environment, ANN};
 
-// #[test]
-// fn xor() {
-//     let config = Config::new(100);
-//     let env = Box::new(XorEnvironment {});
-//     let activation = Activation::Relu;
-//     let mut nn = ANN::new(2, 1, activation);
-//     nn.add_layer(2, Activation::Relu);
-//     let mut cosyne = Cosyne::new(env, nn, config);
-//     for _ in 0..100 {
-//         cosyne.evolve();
-//     }
-//     let champion = cosyne.champion();
-//     println!("champion: {:?}", champion);
-//     assert!(champion.1 > 3.9);
-// }
+#[test]
+fn xor() {
+    let config = Config::new(100);
+    let env = Box::new(XorEnvironment {});
+    let activation = Activation::Relu;
+    let mut nn = ANN::new(2, 1, activation);
+    nn.add_layer(2, Activation::Relu);
+    let mut cosyne = Cosyne::new(env, nn, config);
+    for _ in 0..100 {
+        cosyne.evolve();
+    }
+    let champion = cosyne.champion();
+    println!("champion: {:?}", champion);
+    assert!(champion.1 > 3.9);
+}
 
-// struct XorEnvironment {}
+struct XorEnvironment {}
 
-// impl Environment for XorEnvironment {
-//     fn evaluate(&self, nn: &mut ANN) -> f64 {
-//         let mut distance: f64;
+impl Environment for XorEnvironment {
+    fn evaluate(&self, nn: &mut ANN) -> f64 {
+        let mut distance: f64;
 
-//         let mut output = nn.forward(vec![0.0, 0.0]);
-//         distance = (0.0 - output[0]).abs();
-//         output = nn.forward(vec![0.0, 1.0]);
-//         distance += (1.0 - output[0]).abs();
-//         output = nn.forward(vec![1.0, 0.0]);
-//         distance += (1.0 - output[0]).abs();
-//         output = nn.forward(vec![1.0, 1.0]);
-//         distance += (0.0 - output[0]).abs();
+        let mut output = nn.forward(vec![0.0, 0.0]);
+        distance = (0.0 - output[0]).abs();
+        output = nn.forward(vec![0.0, 1.0]);
+        distance += (1.0 - output[0]).abs();
+        output = nn.forward(vec![1.0, 0.0]);
+        distance += (1.0 - output[0]).abs();
+        output = nn.forward(vec![1.0, 1.0]);
+        distance += (0.0 - output[0]).abs();
 
-//         (4.0 - distance).powi(2)
-//     }
-// }
+        (4.0 - distance).powi(2)
+    }
+}
